f6e181713786027f8d3b541abe1af9e01a1331e7
JNZader <127463636+JNZader@users.noreply.github.com>
2024-03-24 13:40:02 -0300

    Delete BackYFront/hackacode directory

--- BackYFront/hackacode/src/main/java/todocode/hackacode/repos/PaqueteRepository.java
+++
@@ -1,9 +0,0 @@
-package todocode.hackacode.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import todocode.hackacode.domain.Paquete;
-
-@Repository
-public interface PaqueteRepository extends JpaRepository<Paquete, Long> {
-}
